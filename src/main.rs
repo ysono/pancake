fe4acf09aa65d1c47bf433fffaf0331243a9e094
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2021-07-21 19:35:16 -0700

    Unordered test inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use pancake::storage::api::*;
 use pancake::storage::lsm;
+use rand;
+use std::collections::BTreeSet;
 
 fn put(s: &mut lsm::State, k: String, v: Option<String>) {
     s.put(Key(k), v.map(|v| Value::Bytes(v.as_bytes().to_vec())))
@@ -24,16 +26,26 @@ fn get_print(s: &mut lsm::State, k: String) {
 fn main() {
     let mut s = lsm::State::init().unwrap();
 
-    for i in 0..37 {
-        let v = if i % 5 == 0 {
-            None
-        } else {
+    let mut ii = BTreeSet::new();
+
+    for _ in 0..37 {
+        let i = rand::random::<u8>();
+
+        let insert = rand::random::<f32>() < 0.7;
+
+        let key = format!("key{}", i);
+        let val = if insert {
             Some(format!("val{}", i))
+        } else {
+            None
         };
-        put(&mut s, format!("key{}", i), v);
+
+        put(&mut s, key, val);
+
+        ii.insert(i);
     }
 
-    for i in 0..37 {
+    for i in ii.iter() {
         get_print(&mut s, format!("key{}", i));
     }
 }
