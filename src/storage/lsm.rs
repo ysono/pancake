904e669e6faf909106c337911c9c8f9152e452a8
btc <734339+btc@users.noreply.github.com>
2021-07-29 15:25:28 -0400

    move SSTable (#29)

--- src/storage/lsm.rs
+++ src/storage/lsm.rs
@@ -1,19 +1,21 @@
-use super::api::{Key, Value};
-use super::serde;
-use super::utils;
-use crate::storage::serde::KeyValueIterator;
-use anyhow::{anyhow, Result};
-use derive_more::{Deref, DerefMut};
-use itertools::Itertools;
 use std::collections::BTreeMap;
 use std::fs::{self, File, OpenOptions};
-use std::io::{Seek, SeekFrom};
 use std::mem;
 use std::path::PathBuf;
 
+use anyhow::{anyhow, Result};
+use derive_more::{Deref, DerefMut};
+use itertools::Itertools;
+
+use crate::storage::serde::KeyValueIterator;
+
+use super::api::{Key, Value};
+use super::serde;
+use super::utils;
+use crate::storage::sstable::SSTable;
+
 static COMMIT_LOGS_DIR_PATH: &'static str = "/tmp/pancake/commit_logs";
 static SSTABLES_DIR_PATH: &'static str = "/tmp/pancake/sstables";
-static SSTABLE_IDX_SPARSENESS: usize = 3;
 static MEMTABLE_FLUSH_SIZE_THRESH: usize = 7;
 static SSTABLE_COMPACT_COUNT_THRESH: usize = 4;
 
@@ -21,7 +23,7 @@ static SSTABLE_COMPACT_COUNT_THRESH: usize = 4;
 /// Its content corresponds to the append-only commit log.
 /// The memtable and commit log will be flushed to a (on-disk SSTable, in-memory sparse seeks of this SSTable) pair, at a later time.
 #[derive(Default, Deref, DerefMut)]
-struct Memtable(BTreeMap<Key, Value>);
+pub struct Memtable(BTreeMap<Key, Value>);
 
 impl Memtable {
     fn update_from_commit_log(&mut self, path: &PathBuf) -> Result<()> {
@@ -31,100 +33,6 @@ impl Memtable {
             let (key, val) = file_data?;
             self.insert(key, val);
         }
-        Ok(())
-    }
-}
-
-type FileOffset = u64;
-
-/// One SS Table. It consists of a file on disk and an in-memory sparse indexing of the file.
-#[derive(Debug)]
-struct SSTable {
-    path: PathBuf,
-    idx: BTreeMap<Key, FileOffset>,
-}
-
-impl SSTable {
-    fn is_kv_in_mem(kv_i: usize) -> bool {
-        kv_i % SSTABLE_IDX_SPARSENESS == SSTABLE_IDX_SPARSENESS - 1
-    }
-
-    fn write_from_memtable(memtable: &Memtable, path: PathBuf) -> Result<SSTable> {
-        let mut idx = BTreeMap::<Key, FileOffset>::new();
-        let mut file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open(&path)?;
-        let mut offset = 0usize;
-        for (kv_i, (k, v)) in memtable.iter().enumerate() {
-            let delta_offset = serde::serialize_kv(k, v, &mut file)?;
-
-            if SSTable::is_kv_in_mem(kv_i) {
-                idx.insert((*k).clone(), offset as FileOffset);
-            }
-
-            offset += delta_offset;
-        }
-
-        Ok(SSTable { path, idx })
-    }
-
-    fn read_from_file(path: PathBuf) -> Result<SSTable> {
-        let mut idx = BTreeMap::<Key, FileOffset>::new();
-        let mut file = File::open(&path)?;
-        let mut offset = 0usize;
-        for kv_i in 0usize.. {
-            let deser_key = SSTable::is_kv_in_mem(kv_i);
-            match serde::read_kv(&mut file, deser_key, |_| false)? {
-                serde::FileKeyValue::EOF => break,
-                serde::FileKeyValue::KV(delta_offset, maybe_key, _) => {
-                    if let Some(key) = maybe_key {
-                        idx.insert(key, offset as FileOffset);
-                    }
-
-                    offset += delta_offset;
-                }
-            }
-        }
-
-        Ok(SSTable { path, idx })
-    }
-
-    /// Both the in-memory index and the file are sorted by key.
-    /// The index maps { key (sparse) => file offset }.
-    /// 1. Bisect in the in-memory sparse index, to find the lower-bound file offset.
-    /// 1. Seek the offset in the file. Then read linearlly in file until either EOF or the last-read key is greater than the sought key.
-    ///
-    /// @return
-    ///     If found within this sstable, then return Some. The content of the Some may be a tombstone: i.e. Some(Value(None)).
-    ///     If not found within this sstable, then return None.
-    fn search(&self, k: &Key) -> Result<Option<Value>> {
-        // TODO what's the best way to bisect a BTreeMap?
-        let idx_pos = self.idx.iter().rposition(|kv| kv.0 <= k);
-        let file_offset = match idx_pos {
-            None => 0u64,
-            Some(idx_pos) => {
-                let (_, file_offset) = self.idx.iter().nth(idx_pos).unwrap();
-                *file_offset
-            }
-        };
-
-        let mut file = File::open(&self.path)?;
-        file.seek(SeekFrom::Start(file_offset))?;
-
-        loop {
-            match serde::read_kv(&mut file, true, |read_key| read_key == k)? {
-                serde::FileKeyValue::EOF => break,
-                serde::FileKeyValue::KV(_, _, found @ Some(_)) => return Ok(found),
-                _ => continue,
-            }
-        }
-        Ok(None)
-    }
-
-    fn remove_file(&self) -> Result<()> {
-        fs::remove_file(&self.path)?;
         Ok(())
     }
 }
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -2,5 +2,6 @@ pub mod api;
 pub mod lsm;
 pub mod serde;
 pub mod utils;
+pub mod sstable;
 
 pub use lsm::LSM;
