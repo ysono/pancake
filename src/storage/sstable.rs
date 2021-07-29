ee3e6a073825e75edbed4718749c74209874a3ea
btc <734339+btc@users.noreply.github.com>
2021-07-29 13:34:42 -0400

    move SSTable::compact

--- src/storage/sstable.rs
+++ src/storage/sstable.rs
@@ -1,6 +1,6 @@
 use crate::storage::api::{Key, Value};
 use crate::storage::lsm::Memtable;
-use crate::storage::serde;
+use crate::storage::{serde, utils};
 use anyhow::Result;
 use core::option::Option;
 use core::option::Option::{None, Some};
@@ -11,9 +11,12 @@ use std::fs::{File, OpenOptions};
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::path::PathBuf;
+use crate::storage::serde::KeyValueIterator;
+use itertools::Itertools;
 
 type FileOffset = u64;
 
+static SSTABLES_DIR_PATH: &'static str = "/tmp/pancake/sstables";
 static SSTABLE_IDX_SPARSENESS: usize = 3;
 
 /// One SS Table. It consists of a file on disk and an in-memory sparse indexing of the file.
@@ -105,5 +108,75 @@ impl SSTable {
     pub fn remove_file(&self) -> Result<()> {
         fs::remove_file(&self.path)?;
         Ok(())
+    }
+
+    pub fn compact<'a, I: Iterator<Item=&'a Self>>(tables: I) -> Result<Vec<Self>> {
+        let path = utils::timestamped_path(SSTABLES_DIR_PATH);
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&path)?;
+
+        let mut key_value_iterators = Vec::new();
+        for (index, table) in tables.into_iter().enumerate() {
+            let p = &table.path;
+            let file = File::open(p)?;
+
+            // NB: the index/position of the sstable is included for the purpose of breaking ties
+            // on duplicate keys.
+            let iter = KeyValueIterator::from(file).zip(std::iter::repeat(index));
+            key_value_iterators.push(iter);
+        }
+
+        let compacted = key_value_iterators
+            .into_iter()
+            .kmerge_by(|(a, index_a), (b, index_b)| {
+                /*
+                the comparator contract dictates we return true iff |a| is ordered before |b|
+                    or said differently: |a| < |b|.
+
+                for equal keys, we define |a| < |b| iff |a| is more recent.
+                    note: |a| is more recent when index_a > index_b.
+
+                by defining the ordering in this way,
+                    we only keep the first instance of key |k| in the compacted iterator.
+                    duplicate items with key |k| must be discarded.
+                 */
+
+                // guide results to the front of the iterator for early termination
+                if a.is_err() {
+                    return true;
+                }
+                if b.is_err() {
+                    return false;
+                }
+
+                let key_a = &a.as_ref().unwrap().0;
+                let key_b = &b.as_ref().unwrap().0;
+
+                let a_is_equal_but_more_recent = key_a == key_b && index_a > index_b;
+                return key_a < key_b || a_is_equal_but_more_recent;
+            })
+            .map(|a| a.0); // tables[i] is no longer needed
+
+        // Manual impl of:
+        // .unique_by(|(k, _)| k.0.clone()); // keep first instance of |k|
+        let mut prev = None;
+        for result in compacted {
+            let (k, v) = result?;
+            if prev.is_some() && &k == prev.as_ref().unwrap() {
+                continue;
+            }
+            serde::serialize_kv(&k, &v, &mut file)?;
+            prev = Some(k);
+        }
+
+        file.sync_all()?;
+
+        // TODO(btc): instead of |read_from_file|, create SSTable index in streaming fashion
+        let t = Self::read_from_file(path)?;
+
+        Ok(vec![t])
     }
 }
